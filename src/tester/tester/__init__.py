ed9303338aca82f5b66da10b6ef1a4aa2c13aeb3
Gabriel Meghnagi <gabrielmeghnagi@outlook.it>
2020-10-24 17:17:50 +0200

    v0.2

--- src/tester/tester/__init__.py
+++ src/tester/tester/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from tester.view import index, test, start, stop
+from tester.view import index, videos, start, stop, status
 
 
 def create_app(test_config=None):
@@ -8,9 +8,10 @@ def create_app(test_config=None):
     app = Flask('tester', instance_relative_config=True)
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
     app.add_url_rule("/", "index", index, methods=["GET"])
-    app.add_url_rule("/test", "test", test, methods=["GET"])
+    app.add_url_rule("/videos", "videos", videos, methods=["GET"])
     app.add_url_rule("/start", "start", start, methods=["GET"])
     app.add_url_rule("/stop", "stop", stop, methods=["GET"])
+    app.add_url_rule("/status", "status", status, methods=["GET"])
     
     return app
 
--- src/tester/tester/view.py
+++ src/tester/tester/view.py
@@ -2,6 +2,8 @@ import os
 from subprocess import Popen
 from signal import SIGTERM
 from datetime import datetime
+import subprocess
+from random import randint  # DEBUG
 
 from psutil import Process
 from flask import Flask, jsonify, render_template, url_for, session, redirect, request
@@ -12,13 +14,31 @@ def index():
     return render_template('index.html')
 
 
+def get_length(filename):
+    try:
+        result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
+                                 "format=duration", "-of",
+                                 "default=noprint_wrappers=1:nokey=1", filename],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+        result = float(result.stdout)
+        minutes = result / 60
+        if int(minutes) > 0:
+            seconds = result // 60
+            return "{} Minutes, {} Seconds".format(int(minutes), int(seconds))
+        else:
+            return "{} Seconds".format((int(result)))
+    except:
+        return "0 Seconds"
+
+
 def start():
-    # return jsonify({'stopped': 'false'}), 409
+    # return jsonify({'stopped': 'false'}), 202
     if os.path.exists('/opt/bin/ffmpeg.pid'):
         with open('/opt/bin/ffmpeg.pid', 'r') as f:
             parentPid = f.read()
         parentProcess = Process(int(parentPid))
-        return jsonify({'started': 'false', 'status': parentProcess.status()}), 409
+        return jsonify({'started': 'false', 'status': parentProcess.status()}), 202
     parentProcess = Popen(startCmd(), shell=False)
     parentPid = parentProcess.pid
     with open('/opt/bin/ffmpeg.pid', 'w') as f:
@@ -27,30 +47,39 @@ def start():
     return jsonify({'started': 'true', 'status': parentProcess.status()}), 200
 
 
+def status():
+    # return jsonify({'stopped': 'false'}), 202
+    if os.path.exists('/opt/bin/ffmpeg.pid'):
+        return jsonify({'status': 'runnig'}), 200
+    else:
+        return jsonify({'status': 'not runnig'}), 202
+
+
 def stop():
     #return jsonify({'stopped': 'false'}), 200
     try:
         with open('/opt/bin/ffmpeg.pid', 'r') as pidFile:
             parentPid = pidFile.read()
     except:
-            return jsonify({'stopped': 'false'}), 409
+            return jsonify({'stopped': 'false'}), 202
     parentPid = int(parentPid)
     parentProcess = Process(parentPid)
     os.kill(parentPid, SIGTERM)
     os.system("rm -f /opt/bin/ffmpeg.pid")
     return jsonify({'stopped': 'true'}), 200
 
-def test():
+def videos():
     data = {"data": []}
     with os.scandir(r'/opt/bin/tester/tester/static/videos') as dir_contents:
         for entry in dir_contents:
             name = entry.name
+            completeName = '/opt/bin/tester/tester/static/videos/' + name
             if 'mp4' in name:
                 info = entry.stat()
                 size = info.st_size
                 mtime = info.st_mtime
                 date = datetime.fromtimestamp(mtime).strftime('%Y-%m-%d-%H:%M')
-                data["data"].append({"name" : name, "size": round(size / 1024 / 1024, 2), "date": date})
+                data["data"].append({"name" : name, "size": round(size / 1024 / 1024, 2), "date": date, "duration": get_length(completeName)})
     return jsonify(data)
 
 
